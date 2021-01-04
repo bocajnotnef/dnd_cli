ed342fb76e823722b84af2d1a5bf13a22365bfca
Jake Fenton <jake@fenton.io>
2021-01-04 14:49:51 -0500

    The game works!

--- src/console_dice_game.rs
+++ src/console_dice_game.rs
@@ -1,5 +1,62 @@
 use crate::console_utilities::prompt_and_read;
 use crate::dice_game_core;
+use rand::prelude::ThreadRng;
+use rand::Rng;
+
+pub struct RandomPlayer {
+    rand: ThreadRng,
+    description: String,
+}
+
+impl RandomPlayer {
+    const MIN_BET: u16 = 1;
+    const MAX_BET: u16 = 3;
+
+    pub fn new() -> RandomPlayer {
+        let mut the_rng = rand::thread_rng();
+
+        RandomPlayer {
+            description: String::from(format!("A random player ({})", the_rng.gen_range(1, 1000))),
+            rand: the_rng,
+        }
+    }
+}
+
+impl dice_game_core::Player for RandomPlayer {
+    fn get_initial_roll(&mut self) -> u8 {
+        return self.rand.gen_range(1, 7); // d6
+    }
+
+    fn make_initial_bet(&mut self) -> u16 {
+        return self
+            .rand
+            .gen_range(RandomPlayer::MIN_BET, RandomPlayer::MAX_BET + 1);
+    }
+
+    fn react_to_bet(&mut self, current_bet: u16) -> dice_game_core::PlayerAction {
+        match self.rand.gen_range(0, 3) {
+            0 => return dice_game_core::PlayerAction::Fold,
+            1 => return dice_game_core::PlayerAction::Call,
+            2 => return dice_game_core::PlayerAction::Raise(self.rand.gen_range(RandomPlayer::MIN_BET, RandomPlayer::MAX_BET+1)),
+            _ => panic!("This shouldn't be possible, but I don't know how to tell the compiler that the rand range is constrained, sooo")
+        }
+    }
+
+    fn get_meaningful_rolls(&mut self) -> (u8, u8, u8) {
+        return (
+            // 3d6
+            self.rand.gen_range(1, 7),
+            self.rand.gen_range(1, 7),
+            self.rand.gen_range(1, 7),
+        );
+    }
+
+    fn inform_of_result(&self, result: dice_game_core::PlayerResult) {
+        if let dice_game_core::PlayerResult::Won(amount) = result {
+            println!()
+        }
+    }
+}
 
 pub struct ConsoleDiceGame {
   game: dice_game_core::DiceGame,
@@ -10,7 +67,7 @@ impl <'a>ConsoleDiceGame {
     let mut players: Vec<Box<dyn dice_game_core::Player>> = Vec::new();
 
     for _ in 0..num_ai_players {
-      players.push(Box::new(dice_game_core::RandomPlayer::new()));
+      players.push(Box::new(RandomPlayer::new()));
     }
 
     players.push(Box::new(ConsolePlayer{}));
@@ -26,19 +83,25 @@ impl <'a>ConsoleDiceGame {
 
 struct ConsolePlayer {}
 
-impl dice_game_core::Player for ConsolePlayer {
-    fn get_initial_roll(&mut self) -> u8 {
-
-      // get player roll
-      let mut the_roll = prompt_and_read(&String::from("Player's chosen roll"));
-      loop {
-          if (1..7).contains(&the_roll) {
-              break;
-          }
-          the_roll = prompt_and_read(&String::from("Player's chosen roll"));
+impl ConsolePlayer {
+    // TODO: make generic roll
+    // TODO: support multiple rolls for a single 'prompt'
+    fn roll_d6(prompt: &String) -> u8 {
+        let mut the_roll = prompt_and_read(prompt);
+        loop {
+            if (1..7).contains(&the_roll) {
+            break;
+        }
+        the_roll = prompt_and_read(prompt);
       }
 
       the_roll
+    }
+}
+
+impl dice_game_core::Player for ConsolePlayer {
+    fn get_initial_roll(&mut self) -> u8 {
+        ConsolePlayer::roll_d6(&String::from("Player's chosen initial (revealed!) roll"))
     }
 
     fn make_initial_bet(&mut self) -> u16 {
@@ -57,5 +120,20 @@ impl dice_game_core::Player for ConsolePlayer {
           return dice_game_core::PlayerAction::Raise(player_reaction);
         }
       }
+    }
+
+    fn get_meaningful_rolls(&mut self) -> (u8, u8, u8) {
+        (
+            ConsolePlayer::roll_d6(&String::from("Player's first die")),
+            ConsolePlayer::roll_d6(&String::from("Player's second die")),
+            ConsolePlayer::roll_d6(&String::from("Player's third die")),
+        )
+    }
+
+    fn inform_of_result(&self, result: dice_game_core::PlayerResult) {
+        match result {
+            dice_game_core::PlayerResult::Won(amount) => println!("You won {} $money_units!", amount),
+            dice_game_core::PlayerResult::Lost => println!("You lost! Git gud!")
+        }
     }
 }
--- src/dice_game_core.rs
+++ src/dice_game_core.rs
@@ -1,56 +1,31 @@
 use core::panic;
-
-use rand::Rng;
-use rand::prelude::ThreadRng;
+use std::iter::Sum;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum PlayerAction {
     Fold,
     Call,
-    Raise(u16)
+    Raise(u16),
 }
 
+pub enum PlayerResult {
+    Lost,
+    Won(u16), // TODO: refactor w/ currency units
+}
+
+// TODO: put the random generator in like, 'GlobalGame' or 'MetaUniverse' or something
+// currently VERY STUPID that rolling a dice modifies the playerstate
 pub trait Player {
     fn get_initial_roll(&mut self) -> u8;
     // TODO: consider units for bets;
     fn make_initial_bet(&mut self) -> u16;
     fn react_to_bet(&mut self, current_bet: u16) -> PlayerAction;
-}
-
-pub struct RandomPlayer {
-    rand: ThreadRng
-}
-
-impl RandomPlayer {
-    const MIN_BET: u16 = 1;
-    const MAX_BET: u16 = 3;
-
-    pub fn new() -> RandomPlayer {
-        RandomPlayer { rand: rand::thread_rng() }
-    }
-}
-
-impl Player for RandomPlayer {
-    fn get_initial_roll(&mut self) -> u8 {
-        return self.rand.gen_range(1, 7);
-    }
-
-    fn make_initial_bet(&mut self) -> u16 {
-        return self.rand.gen_range(RandomPlayer::MIN_BET, RandomPlayer::MAX_BET+1);
-    }
-
-    fn react_to_bet(&mut self, current_bet: u16) -> PlayerAction {
-        match self.rand.gen_range(0, 3) {
-            0 => return PlayerAction::Fold,
-            1 => return PlayerAction::Call,
-            2 => return PlayerAction::Raise(self.rand.gen_range(RandomPlayer::MIN_BET, RandomPlayer::MAX_BET+1)),
-            _ => panic!("This shouldn't be possible, but I don't know how to tell the compiler that the rand range is constrained, sooo")
-        }
-    }
+    fn get_meaningful_rolls(&mut self) -> (u8, u8, u8); // TODO: better name
+    fn inform_of_result(&self, result: PlayerResult);
 }
 
 pub struct DiceGame {
-    players: Vec<Box<dyn Player>>
+    players: Vec<Box<dyn Player>>,
 }
 
 impl DiceGame {
@@ -63,15 +38,24 @@ impl DiceGame {
 
         // get initial rolls, then sort them to get our betting order
         #[derive(Debug)]
-        struct PlayerRoll { index: usize, roll: u8}
+        struct PlayerRoll {
+            index: usize,
+            roll: u8,
+        }
         let mut initial_rolls: Vec<PlayerRoll> = Vec::new();
         for (position, player_box) in self.players.iter_mut().enumerate() {
-            initial_rolls.push(PlayerRoll{index: position, roll: player_box.get_initial_roll()});
+            initial_rolls.push(PlayerRoll {
+                index: position,
+                roll: player_box.get_initial_roll(),
+            });
         }
         initial_rolls.sort_by(|a, b| a.roll.cmp(&b.roll));
 
         // PHASE TWO: Place all the bets
-        println!("The betting order is: (bet, is_player): {:?}", initial_rolls);
+        println!(
+            "The betting order is: (bet, is_player): {:?}",
+            initial_rolls
+        );
 
         let mut the_bet = self.players[initial_rolls[0].index].make_initial_bet();
         // NOTE: bet order/indexing should match initial_rolls ordering, but this is implicit
@@ -84,8 +68,13 @@ impl DiceGame {
 
         // this isn't as readable as I want it to be
         // TLDR run this loop as long as there's a "raise" present
-        while bets.iter().any(|x| if let PlayerAction::Raise(_) = x { true} else {false}) {
-
+        while bets.iter().any(|x| {
+            if let PlayerAction::Raise(_) = x {
+                true
+            } else {
+                false
+            }
+        }) {
             let mut new_bets: Vec<PlayerAction> = Vec::new();
 
             for (index, action) in bets.iter_mut().enumerate() {
@@ -101,7 +90,9 @@ impl DiceGame {
                     PlayerAction::Fold => new_bets.push(*action),
                     PlayerAction::Call => {
                         let reaction = self.players[index].react_to_bet(the_bet);
-                        if let PlayerAction::Raise(new_bet) = reaction { the_bet = new_bet; }
+                        if let PlayerAction::Raise(new_bet) = reaction {
+                            the_bet = new_bet;
+                        }
                     }
                 }
             }
@@ -110,8 +101,38 @@ impl DiceGame {
         }
 
         // at this point everyone should be a 'call' or 'fold'
-        assert!(!bets.iter().any(|action| if let PlayerAction::Raise(_) = action { true} else {false}), "Raise where there shouldn't be one");
+        assert!(
+            !bets
+                .iter()
+                .any(|action| if let PlayerAction::Raise(_) = action {
+                    true
+                } else {
+                    false
+                }),
+            "Raise where there shouldn't be one"
+        );
 
+        // PHASE 3: figure out who won
+        // TODO: should get the player's other dice rolls @ startup, not here
+
+        let mut final_rolls: Vec<u16> = Vec::new();
+
+        for player in self.players.iter_mut() {
+            let rolls = player.get_meaningful_rolls();
+            final_rolls.push((rolls.0 + rolls.1 + rolls.2).into());
+        }
+
+        let the_pot = bets.iter().fold(0, |acc, bet| if let PlayerAction::Call = bet {acc + the_bet} else {0});
+
+        // https://stackoverflow.com/a/57815298
+        let winning_player_idx = final_rolls.iter().enumerate().max_by(|(_, value0), (_, value1)| value0.cmp(value1)).map(|(idx, _)| idx).unwrap();
+        for (index, player) in self.players.iter().enumerate() {
+            if index == winning_player_idx {
+                player.inform_of_result(PlayerResult::Won(the_pot));
+            } else {
+                player.inform_of_result(PlayerResult::Lost);
+            }
+        }
     }
 
     pub fn rules() -> &'static str {
@@ -120,6 +141,6 @@ impl DiceGame {
 \t* Choose one of the d6 to reveal to the other players.
 \t* Your goal is to have the highest score, using only your remaining 3d6.
 \t* Place bets, in order of descending revealed dice. (i.e., highest first)
-\t* After all bets are settled (e.g. everyone has called/raised/folded), reveal your dice."
+\t* After all bets are settled (e.g. everyone has called/raised/folded), reveal your dice.";
     }
 }
